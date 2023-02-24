04776d913115a69aa2f4cb8d83acced8e1f75caf
Jayliste <jaylisteledama94@gmail.com>
2023-02-24 19:47:42 +0300

    [ROLL] rolled back updates on  music_streaming.py

--- music_streaming/models/music_streaming.py
+++ music_streaming/models/music_streaming.py
@@ -43,45 +43,4 @@ class MusicAlbum(models.Model):
     songs = fields.One2many('music.song', 'album', string="Songs")
 
 
-    @api.model
-    def create(self, vals):
-        record = super(MusicStreaming, self).create(vals)
-        record.mime_type = magic.from_buffer(record.file, mime=True)
-        return record
 
-
-class MusicStreamingMenu(models.Model):
-    _name = 'music.streaming.menu'
-    _description = 'Music Streaming Menu'
-
-    def _get_default_view_id(self):
-        return self.env.ref('music_streaming.music_streaming_tree_view').id
-
-    name = fields.Char(required=True)
-    view_id = fields.Many2one('ir.ui.view', default=_get_default_view_id)
-    
-    @api.model
-    def _update_menuitems(self):
-        action = self.env.ref('music_streaming.music_streaming_form_view')
-        menuitem = self.env.ref('music_streaming.music_streaming_menuitem')
-        menuitem.write({'action': action.id})
-
-
-class MusicStreamingController(models.AbstractModel):
-    _name = 'music.streaming.controller'
-    _inherit = 'website.multi.mixin'
-
-    def list(self, **kwargs):
-        songs = self.env['music.streaming'].search([])
-        return self._show_list(songs)
-
-    def _show_list(self, songs):
-        return self._response('music_streaming.listing', {'songs': songs})
-
-
-class MusicStreamingWebsite(models.Model):
-    _inherit = 'website'
-
-    @http.route('/music_streaming', type='http', auth="public", website=True)
-    def music_streaming(self, **kwargs):
-        return self._call_controller('music.streaming.controller', 'list', **kwargs)
